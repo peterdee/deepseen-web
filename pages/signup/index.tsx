85c7b31fc9d59fc724f047a145fcbbf7506bf7bf
peterdee <p.dyumin@dunice.net>
2021-01-24 20:30:22 +0300

    Add getServerSideProps to the Sign Up page

--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,18 +1,49 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { GetServerSideProps } from 'next';
+import { parse } from 'cookie';
 import { useRouter } from 'next/router';
 
 import {
   BACKEND_URL,
   CLIENT_TYPE,
+  COOKIE_NAME,
   RESPONSE_MESSAGES,
 } from '../../configuration';
 import { Data, Errors } from './types';
+import saveToken from '../../utilities/save-token';
+import setCookie from '../../utilities/set-cookie';
 import styles from './SignUp.module.css';
 
 import LinkButton from '../../components/LinkButton';
 import Loader from '../../components/Loader';
 import SignUpForm from './components/SignUpForm';
+
+export const getServerSideProps: GetServerSideProps = (context): any => {
+  const cookies = context.req.headers.cookie;
+  if (cookies) {
+    try {
+      // make sure that token exists
+      const parsedCookies = parse(cookies);
+      if (parsedCookies && parsedCookies[COOKIE_NAME]) {
+        return {
+          redirect: {
+            destination: '/home',
+            permanent: false,
+          },
+        };
+      }
+    } catch {
+      return {
+        props: {},
+      };
+    }
+  }
+
+  return {
+    props: {},
+  };
+};
 
 export default function SignUp() {
   const [data, setData] = useState<Data>({
@@ -33,8 +64,6 @@ export default function SignUp() {
   const [loading, setLoading] = useState<boolean>(false);
 
   const router = useRouter();
-
-  // TODO: redirect to the Home page if token is present
 
   const handleBackButton = () => router.push('/');
   const handleHaveAccountButton = () => router.push('/signin');
@@ -106,7 +135,11 @@ export default function SignUp() {
         return setFormError('Oops! Something went wrong!');
       }
 
-      // TODO: save data to the Redux store
+      const { token } = responseData;
+
+      // set token as cookie & save token in localStorage
+      setCookie(token);
+      saveToken(token);
 
       return router.push('/home');
     } catch (error) {
