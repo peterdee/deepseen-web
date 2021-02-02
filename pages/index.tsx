b21d3211039e1069d49e948e90f051f8fdfbb9f0
Peter <p.dyumin@dunice.net>
2021-02-02 21:42:09 +0300

    Adjust the Index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { memo } from 'react';
-
 import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+
+import styles from '@/styles/Index.module.css';
 
 function Index() {
   return (
@@ -11,14 +11,19 @@ function Index() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="row">
-        <a href="/signup">
-          Sign up
-        </a>
-        <span className="ml-16 mr-16">|</span>
-        <a href="/signin">
-          Sign in
-        </a>
+      <main className="centered noselect">
+        <h1>
+          Deepseen web application is under construction
+        </h1>
+        <div className="row justify-content-center mt-16">
+          <a href="/signup">
+            Sign up
+          </a>
+          <span className="ml-16 mr-16">|</span>
+          <a href="/signin">
+            Sign in
+          </a>
+        </div>
       </main>
     </div>
   );
